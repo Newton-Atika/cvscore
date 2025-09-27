4ca2a49c785668f27798398f11e1a3f942489a0c
Newton-Atika <163111440+Newton-Atika@users.noreply.github.com>
2025-09-27 19:55:56 +0300

    Update settings.py

--- aicvscore/settings.py
+++ aicvscore/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -80,6 +80,12 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+    )
+}
 
 
 # Password validation
@@ -132,5 +138,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 PAYSTACK_SECRET_KEY = "sk_test_d50b2d43213a91922470eee1eea7fee305c3beff"
+
 
 OPENAI_API_KEY="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"
