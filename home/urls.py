533dc051cbdb828530b37d7c81b0ae7e2164c1c4
Newton-Atika <163111440+Newton-Atika@users.noreply.github.com>
2025-09-28 14:42:46 +0300

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -3,7 +3,8 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path("", views.upload_document, name="upload_document"),
+    path("", views.home, name="home"),   # 👈 now homepage
+    path("upload/", views.upload_document, name="upload_document"),
     path("documents/", views.document_list, name="document_list"),
     path('score/<int:doc_id>/', views.score_cv, name='score_cv'),
     path('signup/', views.signup_view, name='signup'),
@@ -12,3 +13,4 @@ urlpatterns = [
     path('initiate_payment/', views.initiate_payment, name='initiate_payment'),
     path('verify_payment/', views.verify_payment, name='verify_payment'),
 ]
+
